062500c95ebdc1d5c991871c73a43f0281d3e2b8
Anuj Sharma <a.sharma@planittesting.com>
2017-11-10 15:33:16 +1300

    More updates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 from .forms import LoginForm
+from flask import flash,redirect
 
 @app.route('/')
 @app.route('/index')
@@ -25,6 +26,10 @@ def index():
 @app.route('/login',methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html',
                            title='Sign In',
                            form=form)
